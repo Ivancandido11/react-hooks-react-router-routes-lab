0c2fd9494050bfce24c9d7982d89ea39d25f7f6f
Ivan Candido <ivan.candido@powerhrg.com>
2021-07-20 11:04:04 -0400

    Completed Assignment

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,24 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Directors Page</h1>
+        {directors.map(director => {
+          return (
+            <div key={director.name}>
+              <h3>{director.name}</h3>
+              <label> Movies:
+                <ul>
+                  {director.movies.map(movie => 
+                    <li key={movie}>{movie}</li>)}
+                </ul>
+              </label>
+            </div>
+          )
+        })}
+    </div>
+  )
 }
 
 export default Directors;
